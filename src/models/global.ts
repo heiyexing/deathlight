5d2423d7734dcc5a3aa89e0f2e37ad878b692d40
炎熊 <oujinhui.ojh@antgroup.com>
2024-10-10 21:34:07 +0800

    feat: 初始化时间相关 store

--- src/models/global.ts
+++ src/models/global.ts
@@ -1,11 +1,42 @@
+import { useLocalStorageState } from 'ahooks';
 import { useState } from 'react';
 
 const useGlobal = () => {
+  const [totalRecallTime, setTotalRecallTime] = useLocalStorageState(
+    'totalRecallTime',
+    {
+      defaultValue: 30 * 1000,
+    },
+  );
+
+  const [recallTime, setRecallTime] = useLocalStorageState('recallTime', {
+    defaultValue: totalRecallTime,
+  });
+
+  const [totalPauseTime, setTotalPauseTime] = useLocalStorageState(
+    'totalPauseTime',
+    {
+      defaultValue: 60 * 1000,
+    },
+  );
+
+  const [pauseTime, setPauseTime] = useLocalStorageState('pauseTime', {
+    defaultValue: totalPauseTime,
+  });
+
   const [isPaused, setIsPaused] = useState(false);
 
   return {
     isPaused,
     setIsPaused,
+    totalPauseTime,
+    setTotalPauseTime,
+    totalRecallTime,
+    setTotalRecallTime,
+    recallTime,
+    setRecallTime,
+    pauseTime,
+    setPauseTime,
   };
 };
 
